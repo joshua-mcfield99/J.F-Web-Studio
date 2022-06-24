c86ff6bd2a689541e924a5c3e51e6488ccedd2dd
joshua-mcfield99 <72096757+joshua-mcfield99@users.noreply.github.com>
2022-06-24 15:20:35 +0100

    Mobile Nav, Banner, Loader & Type animation

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
+const loader = document.getElementById('preloader');
+const page = document.querySelector('.page');
+
+window.addEventListener('load', function(){
+    loader.style.display = 'none';
+    page.style.display = 'block';
+});
+
 const burger = document.querySelector('.open-menu');
 
 burger.addEventListener('click', (e) =>{
     e.currentTarget.classList.toggle('is-active')
 });
+
