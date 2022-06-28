4d5305074ac9b4db84f0a4de9bbd604de4c10534
joshua-mcfield99 <72096757+joshua-mcfield99@users.noreply.github.com>
2022-06-28 14:27:52 +0100

    Slider fixed

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const page = document.querySelector('.page');
 
 window.addEventListener('load', function(){
     loader.style.display = 'none';
-    page.style.display = 'block';
+    page.style.visibility = 'visible';
+    page.style.height = 'fit-content';
 });
 
 //Hamburger Menu
@@ -15,13 +16,84 @@ burger.addEventListener('click', (e) =>{
     e.currentTarget.classList.toggle('is-active')
 });
 
-//Slider below is the slider im trying to fix
+// Slide
 const track = document.querySelector('.carousel__track');
 const slides = Array.from(track.children);
 const nextButton = document.querySelector('.carousel--right');
 const prevButton = document.querySelector('.carousel--left');
 const dotsNav = document.querySelector('.carousel__nav');
 const dots = Array.from(dotsNav.children);
-//below should return the usual but returns 0 for all
-const slideSize = slides[0].getBoundingClientRect();
-console.log(slideSize);
+const slideWidth = slides[0].getBoundingClientRect().width;
+
+//arange the slides next to one another
+const setSlidePosition = (slide, index) =>{
+    slide.style.left = slideWidth * index + 'px'
+};
+slides.forEach(setSlidePosition);
+
+const moveToSlide = (track, currentSlide, targetSlide) =>{
+    track.style.transform = 'translateX(-' + targetSlide.style.left + ')';
+    currentSlide.classList.remove('current-slide');
+    targetSlide.classList.add('current-slide');
+};
+
+const updateDots = (currentDot, targetDot) =>{
+    currentDot.classList.remove('current-slide');
+    targetDot.classList.add('current-slide');
+};
+
+const hideShowArrows = (slides, prevButton, nextButton, targetIndex) =>{
+    if (targetIndex === 0){
+        prevButton.classList.add('is-hidden');
+        nextButton.classList.remove('is-hidden');
+    } else if (targetIndex === slides.length - 1){
+        nextButton.classList.add('is-hidden');
+        prevButton.classList.remove('is-hidden');
+    } else {
+        prevButton.classList.remove('is-hidden');
+        nextButton.classList.remove('is-hidden');
+    };
+};
+
+//when i click left move slides left
+prevButton.addEventListener('click', e =>{
+    const currentSlide = track.querySelector('.current-slide');
+    const prevSlide = currentSlide.previousElementSibling;
+    const currentDot = dotsNav.querySelector('.current-slide');
+    const prevDot = currentDot.previousElementSibling;
+    const prevIndex = slides.findIndex(slide => slide === prevSlide);
+
+    moveToSlide(track, currentSlide, prevSlide);
+    updateDots(currentDot, prevDot);
+    hideShowArrows(slides, prevButton, nextButton, prevIndex);
+});
+
+//when i click right move slides right
+nextButton.addEventListener('click', e =>{
+    const currentSlide = track.querySelector('.current-slide');
+    const nextSlide = currentSlide.nextElementSibling;
+    const currentDot = dotsNav.querySelector('.current-slide');
+    const nextDot = currentDot.nextElementSibling;
+    const nextIndex = slides.findIndex(slide => slide === nextSlide);
+
+    moveToSlide(track, currentSlide, nextSlide);
+    updateDots(currentDot, nextDot);
+    hideShowArrows(slides, prevButton, nextButton, nextIndex);
+});
+
+//when i click nav indicators move to that slide
+dotsNav.addEventListener('click', e =>{
+    //what indicator was clicked
+    const targetDot = e.target.closest('button');
+
+    if(!targetDot) return;
+
+    const currentSlide = track.querySelector('.current-slide');
+    const currentDot = dotsNav.querySelector('.current-slide');
+    const targetIndex = dots.findIndex(dot => dot === targetDot);
+    const targetSlide = slides[targetIndex];
+
+    moveToSlide(track, currentSlide, targetSlide);
+    updateDots(currentDot, targetDot);
+    hideShowArrows(slides, prevButton, nextButton, targetIndex);
+});
